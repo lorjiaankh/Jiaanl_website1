42e8f7cc3dd0286631fbd5efa8342bfa97873528
lorjiaankh <lorjiaankh@gmail.com>
2024-10-13 18:48:44 +0700

    Delete jiaanlweb directory

--- jiaanlweb/main.js
+++
@@ -1,9 +0,0 @@
-function TimeTeller() {
-    const Time = new Date();
-    
-    const currentdatetime = Time.toLocaleString();
-
-    document.querySelector('#datetime').textContent = currentdatetime;
-}
-
-setInterval(TimeTeller, 1000);
